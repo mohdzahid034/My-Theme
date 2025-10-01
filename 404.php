d13700758924b261ade723d1ecc5790348246f7b
mohdzahid034 <mohdansari0935@gmail.com>
2025-10-01 17:31:25 +0530

    #

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+<h1>404 - Page Not Found</h1>
+<p>Sorry, the page you are looking for does not exist.</p>
+<a href="<?php echo home_url(); ?>">Go back home</a>
+</main>
+
+<?php get_footer(); ?>
