f1b7c658160066bd324b2dffb9f000e3a6c3b8af
mohdzahid034 <mohdansari0935@gmail.com>
2025-10-01 17:05:37 +0530

    #

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 
 <main>
-  
+  <div id="navbar">
+    <div class= "logo">
+      <a href ="#"><img src= "C:\Users\use\Desktop\My-Theme\assets\images\Globe Now.png"></a>
+    </div>
+    <div class ="menu">
+      <li><a href= "#">Home</a></li>
+      <li><a href= "#">Latest</a></li>
+      <li><a href= "#">Categories</a></li>
+      <li><a href= "#">Contact</a></li>
+      <li><a href= "#">About Us</a></li>
+    </div>
+  </div>
 </main>
 
 <?php get_footer(); ?>
