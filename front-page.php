28461b67a46b3983f3f5e1cfe64578d6dad5a2ae
mohdzahid034 <mohdansari0935@gmail.com>
2025-10-01 18:38:42 +0530

    #

--- front-page.php
+++ front-page.php
@@ -49,6 +49,19 @@
       <?php echo do_shortcode(); ?>
     </div>
   </section>
+  <?php
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        ?>
+        <h1><?php the_title(); ?></h1>
+        <div class="page-content">
+            <?php the_content(); ?>
+        </div>
+        <?php
+    }
+}
+?>
 </main>
 
 <?php get_sidebar(); ?>
