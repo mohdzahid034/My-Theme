151ca5d3395ddeedae7ab06a74bdf2b3cc8abb46
mohdzahid034 <mohdansari0935@gmail.com>
2025-10-01 14:37:22 +0530

    check

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <!-- Paste your index.html content here -->
   <h1>Hello, World!</h1>
   <p>This is my custom theme made from HTML/CSS/JS.</p>
+  <p>i am ready</p>
 </main>
 
 <?php get_footer(); ?>
