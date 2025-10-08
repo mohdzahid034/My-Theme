7c184e40c49180b006d1011dbe6e925e6e07ae51
mohdzahid034 <mohdansari0935@gmail.com>
2025-10-08 23:54:30 +0530

    #

--- functions.php
+++ functions.php
@@ -1,22 +1,35 @@
 <?php
-function mytheme_scripts() {
-    // WordPress default stylesheet
-    wp_enqueue_style('style', get_stylesheet_uri(), array(), time());
+function mytheme_enqueue() {
+    // main root style.css
+    wp_enqueue_style( 'mytheme-style', get_stylesheet_uri() );
 
-    // Your custom CSS
-    wp_enqueue_style('custom-css', get_template_directory_uri() . '/assets/css/style.css');
-
-    // Your custom JS
-    wp_enqueue_script('custom-js', get_template_directory_uri() . '/assets/js/functions.js', array(), false, true);
+    // your assets css/js (if you keep them in assets/)
+    wp_enqueue_style( 'mytheme-assets', get_template_directory_uri() . '/assets/css/style.css', array('mytheme-style') );
+    wp_enqueue_script( 'mytheme-js', get_template_directory_uri() . '/assets/js/functions.js', array('jquery'), null, true );
 }
-add_action('wp_enqueue_scripts', 'mytheme_scripts');
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue' );
 
-function mytheme_register_menus() {
-  register_nav_menus([
-    'main-menu' => __( 'Main Menu', 'mytheme' )
-  ]);
+function mytheme_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'html5', array( 'search-form', 'comment-form', 'gallery' ) );
+    register_nav_menus( array(
+        'main-menu' => __( 'Main Menu', 'mytheme' ),
+    ) );
 }
-add_action('init', 'mytheme_register_menus');
+add_action( 'after_setup_theme', 'mytheme_setup' );
+
+function mytheme_widgets_init() {
+    register_sidebar(array(
+        'name'          => 'Sidebar',
+        'id'            => 'sidebar-1',
+        'before_widget' => '<div class="widget %2$s" id="%1$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>'
+    ));
+}
+add_action( 'widgets_init', 'mytheme_widgets_init' );
 
 
 
